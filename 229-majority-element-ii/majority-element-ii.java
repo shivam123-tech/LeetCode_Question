f65f87feda31ce85c82e8581a09a0d55c1ac6551
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2024-10-27 00:56:42 +0530

    Time: 1435 ms (5.59%) | Memory: 48.2 MB (20.20%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> n = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int freq=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    freq++;
+                }
+            }
+            if(freq>nums.length/3){
+                if(n.contains(nums[i])){
+                
+            }else{
+                n.add(nums[i]);
+            }
+            }
+        }
+        return n;
+    }
+}
