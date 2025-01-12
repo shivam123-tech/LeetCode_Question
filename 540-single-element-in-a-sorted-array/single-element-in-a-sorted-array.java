20f6d072f2e3be0ba1c4bd1ac680da621abf237e
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2025-01-12 20:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 50.8 MB (8.31%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,23 +3,34 @@ class Solution {
         if(nums.length==1){
             return nums[0];
         }
-        for(int i=0;i<nums.length;i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
+        int si=0,ei=nums.length-1;
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+            if(mid==0 && nums[mid]!=nums[mid+1]){
+                return nums[mid];
             }
-            else if(i==nums.length-1){
-                if(nums[i-1]!=nums[i]){
-                    return nums[i];
-                }
+            if(mid==nums.length-1 && nums[mid]!=nums[nums.length-2]){
+                return nums[mid];
             }
-            else{
-                if(nums[i-1] != nums[i] && nums[i] != nums[i+1]){
-                return nums[i];
+
+            if(nums[mid-1]!=nums[mid] && nums[mid]!= nums[mid+1]){
+                return nums[mid];
+            }
+
+            if(mid%2==0){
+                if(nums[mid-1]==nums[mid]){
+                    ei=mid-1;
+                }else{
+                    si=mid+1;
+                }
+            } else{
+                if(nums[mid-1]==nums[mid]){
+                    si=mid+1;
+                }else{
+                    ei=mid-1;
                 }
             }
         }
-        return -1;
+         return -1;
     }
 }
