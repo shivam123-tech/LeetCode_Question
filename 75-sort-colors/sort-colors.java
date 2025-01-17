a7c4e9045f229ce6f9cec23ba01a6717c6671e54
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2025-01-17 19:21:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (60.26%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,31 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int si=0;int ei=nums.length-1;
-        quicksort(nums,si,ei);
-    }
-    public static void quicksort(int nums[],int si,int ei){
-        if(si>=ei){
-            return ;
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<nums.length;i++){
+        max=Math.max(max,nums[i]);
+       }
+
+       int count[] = new int[max+1];
+       for(int i=0;i<nums.length;i++){
+        count[nums[i]]++;
+       }
+
+       int j=0;
+       for(int i=0;i<count.length;i++){
+        while(count[i]>0){
+            nums[j]=i;
+            j++;
+            count[i]--;
         }
-        int pIndx=partion(nums,si,ei);
-        quicksort(nums,si,pIndx-1);
-        quicksort(nums,pIndx+1,ei);
-    }
-    public static int partion(int nums[],int si,int ei){
-        int pivot=nums[ei];
-        int i=si-1;
-        for(int j=si;j<ei;j++){
-            if(nums[j]<pivot){
-                i++;
-                int temp=nums[j];
-                nums[j] = nums[i];
-                nums[i]=temp;
-            }
-        }
-        i++;
-        int temp=nums[ei];
-        nums[ei]=nums[i];
-        nums[i]=temp;
-        return i;
+       }
+
     }
 }
