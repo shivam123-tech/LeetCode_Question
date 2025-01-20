d1e82808f2894860cc1f34f4852587c28bc8dd34
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2025-01-21 00:45:41 +0530

    Time: 9 ms (18.52%) | Memory: 43.1 MB (26.81%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,17 +1,16 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        title = title.toLowerCase();
+        title=title.toLowerCase();
         String s[] = title.split(" ");
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length;i++){
             if(s[i].length()<3){
-              sb.append(s[i]+" ");
+                sb.append(s[i]+" ");
             }else{
-                char c = s[i].charAt(0);
-                c=Character.toUpperCase(c);
+                char c=Character.toUpperCase(s[i].charAt(0));
                 sb.append(c+s[i].substring(1)+" ");
             }
         }
-        return sb.toString().trim();
+       return  sb.toString().trim();
     }
 }
