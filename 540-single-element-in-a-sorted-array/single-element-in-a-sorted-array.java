2d2c324e5a67ec22154d498ad06c447c401bc653
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2025-01-23 01:11:13 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (29.16%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int si=0;int ei=nums.length-1;
         if(nums.length==1){
             return nums[0];
         }
-        int si=0,ei=nums.length-1;
         while(si<=ei){
             int mid=si+(ei-si)/2;
             if(mid==0 && nums[mid]!=nums[mid+1]){
@@ -12,25 +12,23 @@ class Solution {
             if(mid==nums.length-1 && nums[mid]!=nums[nums.length-2]){
                 return nums[mid];
             }
-
-            if(nums[mid-1]!=nums[mid] && nums[mid]!= nums[mid+1]){
+            if(nums[mid-1]!=nums[mid] && nums[mid] != nums[mid+1]){
                 return nums[mid];
+            } 
+           if(mid%2==0){
+              if(nums[mid-1]==nums[mid]){
+                ei=mid-1;
+              } else{
+                si=mid+1;
+              }
+           } else{
+            if(nums[mid-1]==nums[mid]){
+                si=mid+1;
+            }else{
+                ei=mid-1;
             }
-
-            if(mid%2==0){
-                if(nums[mid-1]==nums[mid]){
-                    ei=mid-1;
-                }else{
-                    si=mid+1;
-                }
-            } else{
-                if(nums[mid-1]==nums[mid]){
-                    si=mid+1;
-                }else{
-                    ei=mid-1;
-                }
-            }
+           }
         }
-         return -1;
+        return -1;
     }
 }
